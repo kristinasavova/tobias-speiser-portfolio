d8cef1410d43b40aee1e3eeba9e1954529689fe5
Kristina <ccthw@protonmail.com>
2020-06-05 18:35:31 +0300

    Work on skills table

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,29 +6,126 @@ const Table = () => {
             <table>
                 <tbody>
                     <tr>
-                        <td>Maya</td>
-                        <td>Arnold</td>
-                        <td>Substance Painter/Designer</td>
-                        <td>Nuke</td>
+                        <td>
+                            <img 
+                                src='/images/maya-logo.png' 
+                                width='120'
+                                height='auto'
+                                alt='Maya Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/arnold-logo.png' 
+                                width='140'
+                                height='auto'
+                                alt='Arnold Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/s-painter-logo.jpg' 
+                                width='70'
+                                height='auto'
+                                alt='Substance Painter Logo' />
+                        </td>
+                        <td>
+                            <img
+                                src='/images/nuke-logo.png' 
+                                width='120'
+                                height='auto'
+                                alt='Nuke Logo' />
+                        </td>
                     </tr>
                     <tr>
-                        <td>ZBrush</td>
-                        <td>RenderMan</td>
-                        <td>Mari</td>
-                        <td>After Effects</td>
+                        <td>
+                            <img 
+                                src='/images/zbrush-logo.png' 
+                                width='100'
+                                height='auto'
+                                alt='ZBrush Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/renderman-logo.png' 
+                                width='130'
+                                height='auto'
+                                alt='RenderMan Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/s-designer-logo.jpg' 
+                                width='80'
+                                height='auto'
+                                alt='Substance Designer Logo' />
+                        </td>
+                        <td>
+                            <img
+                                src='/images/after-effects.png' 
+                                width='120'
+                                height='auto'
+                                alt='After Effects Logo' />
+                        </td>
                     </tr>
                     <tr>
-                        <td>3DS Max</td>
-                        <td>Redshift</td>
-                        <td>Photoshop</td>
-                        <td>Premiere Pro</td>
+                        <td>
+                            <img 
+                                src='/images/3ds-max-logo.jpg' 
+                                width='140'
+                                height='auto'
+                                alt='3DS Max Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/redshift-logo.png' 
+                                width='130'
+                                height='auto'
+                                alt='Redshift Logo' />
+                        </td>
+                        <td>
+                            <img
+                                src='/images/mari-logo.jpeg' 
+                                width='130'
+                                height='auto'
+                                alt='Mari Logo' />
+                        </td>
+                        <td>
+                            <img
+                                src='/images/premiere-pro.png' 
+                                width='120'
+                                height='auto'
+                                alt='Premiere Pro' />
+                        </td>
                     </tr>
                     <tr>
-                        <td>Blender</td>
-                        <td>Corona</td>
+                        <td>
+                            <img 
+                                src='/images/blender-logo.png' 
+                                width='140'
+                                height='auto'
+                                alt='Blender Logo' />
+                        </td>
+                        <td>
+                            <img 
+                                src='/images/corona-logo.png' 
+                                width='130'
+                                height='auto'
+                                alt='Corona Logo' />
+                        </td>
+                        <td>
+                            <img
+                                src='/images/photoshop-logo.png' 
+                                width='140'
+                                height='auto'
+                                alt='Photoshop Logo' />
+                        </td>
                     </tr>
                     <tr>
-                        <td>Cinema 4D</td>
+                        <td>
+                            <img
+                                src='/images/cinema-4d-logo.png' 
+                                width='100'
+                                height='auto'
+                                alt='Cinema 4D Logo' />
+                        </td>
                     </tr>
                 </tbody>
             </table>
